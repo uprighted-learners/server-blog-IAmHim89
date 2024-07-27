80d2af65df691d44684678933cf4d6ffa1905849
IAmHim89 <computertech0418@gmail.com>
2024-07-27 17:35:29 -0400

    Project

--- controllers/routes.js
+++ controllers/routes.js
@@ -7,7 +7,7 @@ const fs = require("fs");
 const path = require("path");
 
 const myFullPath = path.join(__dirname, "../api/blog.json");
-//?get endpoint display all comments
+//? get endpoint display all comments
 router.get("/project", (req, res) => {
   fs.readFile(myFullPath, "utf8", (err, data) => {
     if (err) {
@@ -24,8 +24,7 @@ router.get("/project", (req, res) => {
     }
   });
 });
-//?get endpoint display 1 comment
-//TODO Makesure to fix post_id single comment display
+//? get endpoint display 1 comment
 router.get("/post_id/:id", (req, res) => {
   fs.readFile(myFullPath, "utf8", (err, data) => {
     if (err) {
@@ -38,7 +37,7 @@ router.get("/post_id/:id", (req, res) => {
         (allComments) => allComments.post_id == postId
       );
 
-      if (allComments) {
+      if (singleComment) {
         res.status(200).json({ singleComment });
       } else {
         req.status(500).send("Error with your comments!!");
@@ -58,11 +57,16 @@ router.post("/create", (req, res) => {
       return res.status(500).send("Something went wrong with your path");
     }
     let jsonData = [];
-    const create = req.body;
+    const create = {
+      post_id: req.body.id,
+      title: req.body.title,
+      author: req.body.author,
+      body: req.body.body,
+    };
     try {
       jsonData = JSON.parse(data);
       jsonData.push(create);
-
+      fs.writeFileSync(myFullPath, JSON.stringify(jsonData));
       res.status(200).json({ jsonData });
     } catch (err) {
       res.status(500).json({
@@ -71,38 +75,60 @@ router.post("/create", (req, res) => {
     }
   });
 });
+//? patch endpoint to update comment
 router.patch("/update/:id", (req, res) => {
-  fs.readFile(myFullPath, "utf8", async (err, data) => {
+  fs.readFile(myFullPath, "utf8", (err, data) => {
     if (err) {
       return res.status(500).send("Error with connecting Path");
     }
+
     try {
-      const update = req.body;
-      const id = req.params.id;
-      let jsonBlog = JSON.parse(data);
+      const jsonBlog = JSON.parse(data);
+
+      let jsonIndex = jsonBlog.findIndex(
+        (jsonBlog) => jsonBlog.post_id == req.params.id
+      );
+
+      jsonBlog[jsonIndex] = {
+        post_id: req.body.id,
+        title: req.body.title,
+        author: req.body.author,
+        body: req.body.body,
+        text: req.body.text,
+      };
+      res.status(200).json({
+        updated: jsonBlog[jsonIndex],
+        Results: jsonBlog,
+      });
+      fs.writeFileSync(myFullPath, JSON.stringify(jsonBlog));
+      console.log(jsonIndex);
     } catch (error) {
       res.status(500).send("Server not Working!");
     }
   });
 });
-router.delete("/project", (req, res) => {});
+router.delete("/remove/:id", (req, res) => {
+  fs.readFile(myFullPath, "utf8", (err, data) => {
+    if (err) throw res.status(500).send("Something went wrong with file Path");
+    try {
+      const jsonBlog = JSON.parse(data);
+      let jIndex = jsonBlog.findIndex(
+        (jsonBlog) => jsonBlog.post_id == req.params.id
+      );
+      jsonBlog.splice(jIndex, 1);
+      fs.writeFileSync(myFullPath, JSON.stringify(jsonBlog));
+      res.status(200).json({
+        Deleted: 1,
+        Results: jsonBlog,
+      });
+    } catch (err) {
+      res.status(500).json({
+        Error: err.message,
+      });
+    }
+  });
+});
 
 module.exports = router;
 
-/* 
-
-fs.readFile(path, "utf8", (err, data) => {
-    // condtitional to check setup works properlf through fs
-    if (err) {
-      return res.status(500).send("Internal Server Error");
-    }
-    //try catch for parse json data trial and error
-    try {
-      const jsonData = JSON.parse(data);
-      res.status(200).json(jsonData);
-    } catch (Err) {
-      res.status(500).send("Error parsing JSON data:");
-    }
-  });
-
-*/
+// // const update = req.body;
